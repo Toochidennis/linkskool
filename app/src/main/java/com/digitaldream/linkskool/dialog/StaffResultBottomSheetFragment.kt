a0960713a9e7a039df66f1a1b9a73e85e604b30f
Toochidennis <dennistoochukwu@gmail.com>
2023-11-22 16:46:47 +0100

    Updated class

--- app/src/main/java/com/digitaldream/linkskool/dialog/StaffResultBottomSheetFragment.kt
+++ app/src/main/java/com/digitaldream/linkskool/dialog/StaffResultBottomSheetFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.digitaldream.linkskool.R
-import com.digitaldream.linkskool.activities.StaffAttendanceActivity
+import com.digitaldream.linkskool.activities.AttendanceActivity
 import com.digitaldream.linkskool.activities.StaffEnterResult
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
@@ -15,6 +15,7 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 private const val ARG_PARAM3 = "param3"
+private const val ARG_PARAM4 = "param4"
 
 
 class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
@@ -26,6 +27,7 @@ class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
     private var courseId: String? = null
     private var levelId: String? = null
     private var classId: String? = null
+    private var courseName: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +35,7 @@ class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
             courseId = it.getString(ARG_PARAM1)
             levelId = it.getString(ARG_PARAM2)
             classId = it.getString(ARG_PARAM3)
+            courseName = it.getString(ARG_PARAM4)
         }
     }
 
@@ -47,12 +50,13 @@ class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
     companion object {
 
         @JvmStatic
-        fun newInstance(levelId: String, courseId: String, classId: String) =
+        fun newInstance(levelId: String, courseId: String, classId: String, courseName: String) =
             StaffResultBottomSheetFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, levelId)
                     putString(ARG_PARAM2, courseId)
                     putString(ARG_PARAM3, classId)
+                    putString(ARG_PARAM4, courseName)
                 }
             }
     }
@@ -83,7 +87,7 @@ class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
         }
 
         attendanceBtn.setOnClickListener {
-            launchActivity("course_attendance", StaffAttendanceActivity::class.java)
+            launchActivity("course_attendance", AttendanceActivity::class.java)
         }
 
     }
@@ -95,6 +99,7 @@ class StaffResultBottomSheetFragment : BottomSheetDialogFragment() {
                 .putExtra("course_id", courseId ?: "")
                 .putExtra("level_id", levelId ?: "")
                 .putExtra("class_id", classId ?: "")
+                .putExtra("course_name", courseName ?: "")
         )
 
         dismiss()
