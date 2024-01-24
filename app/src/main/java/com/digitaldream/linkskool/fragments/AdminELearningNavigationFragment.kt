dfb49fa4ab18d5a75d7bd85e56f4b877802ce67c
Toochidennis <dennistoochukwu@gmail.com>
2024-01-24 10:43:00 +0100

    update

---
+++ app/src/main/java/com/digitaldream/linkskool/fragments/AdminELearningNavigationFragment.kt
@@ -0,0 +1,7 @@
+package com.digitaldream.linkskool.fragments
+
+import androidx.fragment.app.Fragment
+import com.digitaldream.linkskool.R
+
+
+class AdminELearningNavigationFragment : Fragment(R.layout.fragment_admin_e_learning_navigation)
