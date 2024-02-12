0d1953bc71eb6205a05166e6045f672da35fbe8f
Toochidennis <dennistoochukwu@gmail.com>
2024-02-12 16:59:42 +0100

    update

--- app/src/main/java/com/digitaldream/linkskool/activities/Settings.java
+++ app/src/main/java/com/digitaldream/linkskool/activities/Settings.java
@@ -1,6 +1,7 @@
 package com.digitaldream.linkskool.activities;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ListView;
@@ -10,12 +11,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.digitaldream.linkskool.R;
 import com.digitaldream.linkskool.adapters.SettingListAdapter;
+import com.digitaldream.linkskool.config.DatabaseHelper;
+import com.digitaldream.linkskool.models.CourseOutlineTable;
+import com.digitaldream.linkskool.models.ExamType;
+import com.digitaldream.linkskool.models.NewsTable;
+import com.digitaldream.linkskool.models.StudentCourses;
+import com.digitaldream.linkskool.models.StudentResultDownloadTable;
+import com.digitaldream.linkskool.models.StudentTable;
+import com.digitaldream.linkskool.models.VideoTable;
+import com.digitaldream.linkskool.models.VideoUtilTable;
+import com.j256.ormlite.table.TableUtils;
 
+import java.sql.SQLException;
 import java.util.Objects;
 
 public class Settings extends AppCompatActivity {
     private final String[] settingsTitle = {"General settings", "Level", "Courses",
-            "Grade", "Assessment", "Class"};
+            "Grade", "Assessment", "Class", "Login"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +62,50 @@ public class Settings extends AppCompatActivity {
                 case 4:
                     startActivity(new Intent(this, AssessmentSetting.class));
                     break;
-/*
-                case 5:
-                    startActivity(new Intent(this, AddClass.class));
-                    break;*/
+                case 6:
+                    logout();
+                    break;
             }
         });
+    }
+
+    private void logout() {
+        SharedPreferences sharedPreferences = getSharedPreferences(
+                "loginDetail", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("loginStatus", false);
+        editor.putString("user", "");
+        editor.putString("school_name", "");
+        editor.putString("attachment", "");
+        editor.apply();
+
+        try {
+            DatabaseHelper databaseHelper = new DatabaseHelper(this);
+
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    StudentTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    NewsTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    StudentCourses.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    StudentResultDownloadTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    VideoTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    VideoUtilTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    CourseOutlineTable.class);
+            TableUtils.clearTable(databaseHelper.getConnectionSource(),
+                    ExamType.class);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
     }
 
     @Override
